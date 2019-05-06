b229abe289940cee2fc7766153b022db8629ec8f
YuqianHou <50307487+YuqianHou@users.noreply.github.com>
2019-05-06 16:26:33 -0700

    Add files via upload

--- IntegerHashes.cpp
+++ IntegerHashes.cpp
@@ -1,7 +1,5 @@
 #include "IntegerHashes.h"
-#include <iostream>
 #include <cmath>
-#include <cstdint>
 
 IntegerHash::IntegerHash(uint64_t i, uint64_t m) {
     this->i = i;
@@ -9,50 +7,30 @@ IntegerHash::IntegerHash(uint64_t i, uint64_t m) {
 }
 
 DivisionHash::DivisionHash(uint64_t i, uint64_t m): IntegerHash(i, m) {
-    // Nothing else to do
+    // Nothing else to do.
 }
 
-/**
- * This function uses the "division method" of hashing
- * It first multiplies the input by i + 1, then reduces the result modulo m
- */
-uint64_t DivisionHash::hash(uint64_t input) const{
+uint64_t DivisionHash::hash(uint64_t input) const {
     return input * (i + 1) % m;
 }
 
-
-/**
- * To initial the value of b
- */
 ReciprocalHash::ReciprocalHash(uint64_t i, uint64_t m): IntegerHash(i, m) {
     this->b = double(1) / (i + 2);
 }
 
-/**
- * This function uses the "multiplication method" of hashing
- * It multiplies the input by 1 / (i + 2), then multiplies the fractional part by m
- */
-uint64_t ReciprocalHash::hash(uint64_t input) const{
-    double temp = input * b;
+uint64_t ReciprocalHash::hash(uint64_t input) const {
+    double temp = b * input;
     temp -= std::floor(temp);
     return temp * m;
 }
 
-
-/**
- * To initial the value of b
- */
 SquareRootHash::SquareRootHash(uint64_t i, uint64_t m): IntegerHash(i, m) {
     this->b  = std::sqrt(4 * i + 3);
     this->b -= std::floor(this->b);
 }
 
-/**
- * This function also uses the "multiplication method" of hashing
- * It multiplies the input by the square root of 4i + 3, then multiplies the fractional part by m
- */
-uint64_t SquareRootHash::hash(uint64_t input) const{
-    double temp = input * b;
+uint64_t SquareRootHash::hash(uint64_t input) const {
+    double temp = b * input;
     temp -= std::floor(temp);
     return temp * m;
 }
--- StringHashes.cpp
+++ StringHashes.cpp
@@ -1,9 +1,7 @@
 #include "StringHashes.h"
-#include <iostream>
-#include <cstdint>
 
-// From https://en.wikipedia.org/wiki/Jenkins_hash_function
-uint64_t JenkinsHash::hash(const std::string& input) const{
+// https://en.wikipedia.org/wiki/Jenkins_hash_function
+uint64_t JenkinsHash::hash(const std::string& input) const {
     uint32_t hash = 0;
     for(uint8_t byte: input) {
         hash += byte;
@@ -17,8 +15,8 @@ uint64_t JenkinsHash::hash(const std::string& input) const{
     return hash;
 }
 
-// From https://en.wikipedia.org/wiki/Jenkins_hash_function
-uint64_t PearsonHash::hash(const std::string& input) const{
+// https://en.wikipedia.org/wiki/Pearson_hashing
+uint64_t PearsonHash::hash(const std::string& input) const {
     uint64_t hash = 0;
     for(uint8_t byte: input) {
         hash = 255 - (hash ^ byte);
